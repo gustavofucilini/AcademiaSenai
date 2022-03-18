73983b7b93805ea5d0aa7e6a1576061426b760c1
Gustavo Fucilini <gustavofucilini@gmail.com>
2022-03-17 22:50:21 -0300

    Adicionado novo atributo

--- src/br/com/senai/academiasenai/entity/Professor.java
+++ src/br/com/senai/academiasenai/entity/Professor.java
@@ -1,9 +1,12 @@
 package br.com.senai.academiasenai.entity;
 
+import java.time.LocalDate;
+
 public class Professor {
 
 	private Integer id;
 	private String nomeCompleto;
+	private LocalDate dataDeAdmicao;
 
 	public Integer getId() {
 		return id;
@@ -19,6 +22,14 @@ public class Professor {
 
 	public void setNomeCompleto(String nomeCompleto) {
 		this.nomeCompleto = nomeCompleto;
+	}
+
+	public LocalDate getDataDeAdmicao() {
+		return dataDeAdmicao;
+	}
+
+	public void setDataDeAdmicao(LocalDate dataDeAdmicao) {
+		this.dataDeAdmicao = dataDeAdmicao;
 	}
 
 }
